d22abfbfe0854607f98778d35ab586aa265ac1d6
sanket <sanket.r.salve@gmail.com>
2025-03-13 16:39:10 +0530

    works on netlify prod

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const serverless = require("serverless-http");
 const path = require('path');
 const uploadRoutes = require('./routes/uploadRoutes');
 // Initialize Express app
 const app = express();
+// Enable CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+  
 
 // Serve Static Files
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Use Routes
+app.use(express.json());
 app.use('/api', uploadRoutes);
 
-// Use serverless for netlify deploy
-
-module.exports.handler = serverless(app);
+module.exports = app;
